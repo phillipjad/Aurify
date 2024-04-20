a4b1291efc13459c3bc661472d49c38f172c0ff3
phillipjad <phillipjad@gmail.com>
2024-04-19 22:37:37 -0400

    Frontend skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Container, Row } from 'react-bootstrap'
+import Header from './components/home/Header'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
+
+export default App
